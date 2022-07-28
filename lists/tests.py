708bc11e23e508aa7a94e17ea5b8fa3b3df8bdca
ivanpravada <vano.provada@gmail.com>
2022-07-28 19:39:48 +0200

    Добавлена обработка post-запросов и продолжены FT

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.test import TestCase
 
 class HomePageTest(TestCase):
@@ -7,4 +8,11 @@ class HomePageTest(TestCase):
         '''тест: используется домашний шаблон'''
 
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        '''тест: можно сохранить post-запрос'''
+
+        response = self.client.post('/', data = {'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
