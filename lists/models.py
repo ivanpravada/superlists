61b301a6f691c4fc760cfa0b6b1299b73135baf7
ivanpravada <vano.provada@gmail.com>
2022-08-07 15:36:52 +0200

    list related to item

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+class List(models.Model):
+    '''список'''
+    pass
+
 class Item(models.Model):
-    '''элемент списка'''
+    '''элемент'''
 
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.test import TestCase
 
 class HomePageTest(TestCase):
@@ -33,26 +33,36 @@ class HomePageTest(TestCase):
     #     self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     '''тест модели элемента списка'''
 
     def test_saving_and_retrieving_items(self):
         '''тест сохранения и получения элементов списка'''
 
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
     # def test_display_all_list_items(self):
     #     '''тест: отображаются все элементы списка'''
@@ -70,15 +80,16 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         '''тест: используется шаблон списка'''
-
+        
         response = self.client.get('/lists/the_best_link/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_list_items(self):
         '''тест: отображаются все элементы списка'''
 
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
         
         response = self.client.get('/lists/the_best_link/')
 
