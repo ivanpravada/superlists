5d4aa5e15a601e7fcf0f4bb5104882b0cf3eba21
ivanpravada <vano.provada@gmail.com>
2022-08-08 20:07:46 +0200

    functions done

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
+from django.conf.urls import include
 from django.urls import re_path
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    re_path(r'^$', views.home_page, name='home'),
-    re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'lists/(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    re_path(r'^$', list_views.home_page, name='home'),
+    re_path(r'^lists/', include(list_urls)),
 ]
