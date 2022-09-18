b8c2b329a638cf2da4835675714f7ccf4c5f3328
ivanpravada <vano.provada@gmail.com>
2022-09-18 19:19:52 +0200

    Скорректировано новое представление списка с учетом валидации модели

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
+import re
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 
 def home_page(request):
@@ -17,7 +19,14 @@ def new_list(request):
     '''новый список'''
     
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:    
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
