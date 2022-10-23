9af21b4050a5c4df64c64b76bb959643bc1120af
ivanpravada <vano.provada@gmail.com>
2022-10-23 19:41:56 +0200

    Реализована валидация наличия повторяющегося элемента на уровне модели

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 from django.urls import reverse
 
@@ -13,3 +14,10 @@ class Item(models.Model):
 
     text = models.TextField(default='')
     list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self) -> str:
+        return self.text
