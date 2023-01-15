ca7fab5ff2730501d4043e2b31c7cc5b9fdf497e
ivanpravada <vano.provada@gmail.com>
2023-01-15 18:15:16 +0100

    Added authenticate function

--- accounts/tests/test_view.py
+++ accounts/tests/test_view.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from unittest.mock import patch, call
+from accounts.models import Token
 
 class SendLoginEmailViewTest(TestCase):
     '''тест представления, которое отправляет сообщение для входа в систему'''
@@ -10,3 +12,62 @@ class SendLoginEmailViewTest(TestCase):
             'email': 'edith@example.com'
         })
         self.assertRedirects(response, '/')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
+        '''тест: отправляется сообщение на адрес из метода post'''
+
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['edith@example.com'])
+    
+    def test_adds_success_message(self):
+        '''тест: добавляется сообщение об успехе'''
+
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        }, follow=True)
+
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, 'success')
+
+class LoginViewTest(TestCase):
+    '''тест представления входа в систему'''
+
+    def test_redirect_to_home_page(self):
+        '''тест: переадресация на домашнюю страницу'''
+
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(response, '/')
+
+    def test_creates_token_associated_with_email(self):
+        '''тест: создается токен, связанный с email'''
+
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'edith@example.com')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token_id(self, mock_send_mail):
+        '''тест: отсылается ссылка на вход в систему, используя uid токена'''  
+
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        token = Token.objects.first()
+        expected_url = f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
