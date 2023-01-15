f8408b10dd08ccfedc6d0b5c196eef95fb12d215
ivanpravada <vano.provada@gmail.com>
2023-01-15 18:15:53 +0100

    Added authenticate function test

---
+++ accounts/authentication.py
@@ -0,0 +1,14 @@
+from accounts.models import User, Token
+
+class PasswordlessAuthenticationBackend(object):
+    '''беспарольный серверный процессор аутентификации'''
+
+    def authenticate(self, uid):
+        '''аутентифицировать'''
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
+        except Token.DoesNotExist:
+            return None
