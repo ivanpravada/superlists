69636a726a7db467b2c9f111d070dd185b916a8d
ivanpravada <vano.provada@gmail.com>
2023-01-29 16:10:42 +0100

    Представление new_list пытается присвоить владельца, но не может

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,13 +1,14 @@
 from glob import escape
-from urllib import response
 from lists.models import Item, List
 from django.test import TestCase
 from django.utils.html import escape
+from django.contrib.auth import get_user_model
 from lists.forms import (
     DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
     ExistingListItemForm, ItemForm,
 )
-from unittest import skip
+
+User = get_user_model()
 
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
@@ -188,11 +189,30 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        '''тест: владелец сохраняется если пользователь аутентифицирован'''
+
+        user = User.objects.create(email='a@b.com')
+        self.client.force_login(user)
+        self.client.post('/lists/new', data={'text': 'new item'})
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
+        
+
 class MyListsTest(TestCase):
     '''тест приложения "My Lists"'''
 
     def test_my_lists_url_renders_my_lists_template(self):
         '''тест: используется шаблон my_lists'''
 
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        '''тест: передается правильный владелец в шаблон'''
+
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,9 @@
-from cgitb import text
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 from lists.forms import ItemForm, ExistingListItemForm
 from lists.models import List
 
+User = get_user_model()
 
 def home_page(request):
     '''домашняя страница'''
@@ -26,7 +27,9 @@ def new_list(request):
     
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -35,4 +38,5 @@ def new_list(request):
 def my_lists(request, email):
     '''Мои списки'''
 
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
