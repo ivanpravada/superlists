27762b865d39afc5bf5321a7dde0d5a0aed43abe
ivanpravada <vano.provada@gmail.com>
2023-02-19 20:29:57 +0100

    Добавлен скриншот на неполадки ФТ

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,9 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 from .server_tools import reset_database
+from datetime import datetime
 import os
 import time
 
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 MAX_WAIT = 10
 
@@ -23,7 +28,49 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         '''демонтаж'''
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to.window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+        '''тест не сработал'''
+
+        return any(error for (method, error) in self._outcome.errors)
+
+    def take_screenshot(self):
+        '''сделать снимок экрана'''
+
+        filename = self._get_filename() + '.png'
+        print('screenshotting to ', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        '''выгрузить html'''
+
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to ', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        '''получить имя файла'''
+
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.\
+            format(
+                folder=SCREEN_DUMP_LOCATION,
+                classname=self.__class__.__name__,
+                method=self._testMethodName,
+                windowid=self._windowid,
+                timestamp=timestamp
+            )
 
     def wait(fn):
         def modified_fn(*args, **kwargs):
