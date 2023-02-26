c88465604aa08bd2f11a3c7196290b961a9a9663
ivanpravada <vano.provada@gmail.com>
2023-02-26 14:44:53 +0100

    REST API, AJAX tests

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import include
-from django.views.generic.base import RedirectView
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import re_path
 from lists import views as list_views
 from lists import urls as list_urls
+from lists import api_urls
 from accounts import urls as accounts_urls
 
 urlpatterns = [
     re_path(r'^$', list_views.home_page, name='home'),
     re_path(r'^lists/', include(list_urls)),
     re_path(r'^accounts/', include(accounts_urls)),
+    re_path(r'^api/', include(api_urls)),
 ]
